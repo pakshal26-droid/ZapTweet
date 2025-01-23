c3a0ccb554c59064c479283aad01e6b227d77e73
pakshal26-droid <pakshaljain54@gmail.com>
2025-01-24 01:20:43 +0530

    made changes

--- src/components/landingpage/LandingHero.js
+++ src/components/landingpage/LandingHero.js
@@ -4,16 +4,16 @@ import 'react-tooltip/dist/react-tooltip.css';
 
 const LandingHero = ({onGetStarted}) => {
   return (
-    <div className='h-[80vh] font-anek max-w-5xl mx-auto flex flex-col items-center justify-center'>
-      <h1 className='text-6xl font-black text-center'>Create and Schedule Quality<span className='font-caladea tracking-tighter font-medium italic'> Linkedin </span> and <span className='font-caladea tracking-tighter font-medium italic'>    X  </span>  posts within a few clicks</h1>
+    <div className='md:h-[80vh] h-[90vh] font-anek max-w-5xl mx-auto flex flex-col items-center justify-center'>
+      <h1 className='lg:text-6xl text-3xl font-black text-center'>Create and Schedule Quality<span className='font-caladea tracking-tighter font-medium italic'> Linkedin </span> and <span className='font-caladea tracking-tighter font-medium italic'>    X  </span>  posts within a few clicks</h1>
       <div className='pt-5 mx-auto'>
-        <ul className='list-disc list-inside text-gray-600 text-2xl tracking-normal gap-x-10 flex flex-row justify-between items-center '>
+        <ul className='list-disc list-inside text-gray-600 text-sm lg:text-2xl tracking-normal gap-x-10 flex flex-col md:flex-row justify-between items-center '>
             <li>One click generation</li>
             <li>Personalised creation</li>
             <li>Performance Optimised</li>
         </ul>
       </div>
-      <button className='bg-blue-600 mt-10  text-white hover:bg-white hover:text-black border-2 border-black px-20 py-5 text-2xl font-semibold rounded-full' onClick={onGetStarted}>Start Creating</button>
+      <button className='bg-blue-600 mt-10  text-white hover:bg-white hover:text-black border-2 border-black px-20 py-2 lg:py-5 text-md lg:text-2xl font-semibold rounded-full' onClick={onGetStarted}>Start Creating</button>
       </div>
   );
 };
--- src/components/landingpage/LandingNavbar.js
+++ src/components/landingpage/LandingNavbar.js
@@ -5,8 +5,8 @@ const LandingNavbar = ({onGetStarted}) => {
   return (
     <div>
       <div className='flex flex-row items-center h-auto my-auto justify-between'>
-        <h1 className='text-3xl font-caladea tracking-tighter font-black'>ZapTweet</h1>
-        <button className='bg-blue-600 font-semibold text-white hover:bg-white hover:text-black border-2 border-black px-10 py-2 rounded-full' onClick={onGetStarted}>Get Started </button>
+        <h1 className='lg:text-3xl text-xl font-caladea tracking-tighter font-black'>ZapTweet</h1>
+        <button className='bg-blue-600 font-semibold text-white hover:bg-white hover:text-black border-2 border-black px-7 lg:px-10 py-1 lg:py-2 rounded-full' onClick={onGetStarted}>Get Started </button>
       </div>
     </div>
   );
